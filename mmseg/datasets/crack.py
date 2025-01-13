fb0b6790380a5093385f3395aafe148221bfdb5a
SALTYF1SH <3238377965@qq.com>
2025-01-13 11:47:41 +0800

    [modify] crack dataset

--- mmseg/datasets/crack.py
+++ mmseg/datasets/crack.py
@@ -8,23 +8,15 @@ from .basesegdataset import BaseSegDataset
 
 
 @DATASETS.register_module()
-class PascalVOCDataset(BaseSegDataset):
+class CrackDataset(BaseSegDataset):
     """Pascal VOC dataset.
 
     Args:
         split (str): Split txt file for Pascal VOC.
     """
     METAINFO = dict(
-        classes=('background', 'aeroplane', 'bicycle', 'bird', 'boat',
-                 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
-                 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep',
-                 'sofa', 'train', 'tvmonitor'),
-        palette=[[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
-                 [0, 0, 128], [128, 0, 128], [0, 128, 128], [128, 128, 128],
-                 [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
-                 [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
-                 [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
-                 [0, 64, 128]])
+        classes=('background', 'crack'),
+        palette=[[0, 0, 0], [128, 0, 0]])
 
     def __init__(self,
                  ann_file,
